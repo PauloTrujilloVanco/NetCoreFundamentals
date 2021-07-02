7bc69f30d9c23445a5995e1adb021cc9c997f1dd
Paulo Trujillo <paulo.trujillo@vanco.com>
2021-07-02 12:38:18 -0500

    Ads more restaurants and cuisine types

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -7,6 +7,7 @@ namespace OdeToFood.Data
     public interface IRestaurantData
     {
         IEnumerable<Restaurant> GetRestaurantsByName(string name);
+        Restaurant GetById(int id);
     }
 
     public class InMemoryRestaurantData : IRestaurantData
@@ -20,10 +21,15 @@ namespace OdeToFood.Data
                 new Restaurant { Id=1, Name="Pizza Renzo", Location="Playa del Carmen", Cuisine= CuisineType.Italian},
                 new Restaurant { Id=2, Name="Carnes Garibaldi", Location="Guadalajara", Cuisine= CuisineType.Mexican},
                 new Restaurant { Id=3, Name="Al Meraj", Location="Zapopan", Cuisine= CuisineType.Indian},
-
+                new Restaurant { Id=4, Name="Al Malek", Location="Guadalajara", Cuisine= CuisineType.Libanese},
+                new Restaurant { Id=5, Name="La Vaca Argentina", Location="Guadalajara", Cuisine= CuisineType.Argentinian},
             };
         }
 
+        public Restaurant GetById(int id)
+        {
+            return restaurants.SingleOrDefault(r => r.Id == id);
+        }
 
         public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
         {
